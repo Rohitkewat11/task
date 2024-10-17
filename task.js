2cd9624ad582a796035cd5bfda8c6f84d8fddc43
Rohitkewat11 <133211337+Rohitkewat11@users.noreply.github.com>
2024-10-17 18:51:40 +0530

    first commit

---
+++ task.js
@@ -0,0 +1,57 @@
+// let value = 56895423;
+// const range = [2000,1000,500,200,100,50,20,10,5,2,1];
+
+// let res;
+// let mod;
+// const result = [];
+// for(i=0;i<=range.length;i++){
+//     if(value >= range[i]){
+//         mod = (value % range[i]);
+//         res = Math.floor(value/range[i])
+//         value=mod;
+//         console.log(range[i], "*" ,res ,"=",range[i]*res );
+//     }
+// }
+
+// ////////////////////////////////////
+// const mean = [5,6,7,8,9];
+// var res = 0;
+// for(i=0;i<mean.length;i++){
+//     res = res+mean[i];
+// }
+// console.log(res/mean.length);
+
+// ///////////////////////////////////////////////
+// const medium = [5, 2, 8,1,6, 9, 4,9];
+// for (i = 0; i < medium.length; i++) {
+//   for (j = 0; j < medium.length; j++) {
+//     if (medium[i] < medium[j]) {
+//       [medium[i], medium[j]] = [medium[j], medium[i]];
+//     }
+//   }
+// }
+
+// console.log(medium);
+
+// if (medium.length % 2 !== 0) {   // for odd values length
+//   res = medium.length / 2;
+//   console.log("odd",medium[res]);
+// }else{                              // for even values length
+//     res = medium.length / 2;
+//     console.log(`even ${medium[res-1]},${medium[res]}`);
+// }
+
+// const mode = [2,5,6,6,6,62,4,8,7];
+// const result = [];
+// for(i=0;i<mode.length;i++){
+//     let count=0;
+//     let inc = count;
+//     for(j=0;j<mode.length;j++){
+//         if(mode[i] == mode[j]){
+//             count++;
+//         }
+//     }
+//             result.push(`${mode[i]}:${count}`);
+// }
+// console.log(result);
+
